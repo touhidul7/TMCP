72e213c65175537ab54ecc718539463fd5341cda
touhidul7 <touhidul2@hotmail.com>
2026-06-28 20:50:17 +0600

    analytics and speedinsights packages added

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,6 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { MockStoreProvider } from "@/lib/mock-store";
 
 export const metadata = {
@@ -13,6 +15,8 @@ export default function RootLayout({ children }) {
         <MockStoreProvider>
           {children}
         </MockStoreProvider>
+        <Analytics />
+        <SpeedInsights/>
       </body>
     </html>
   );
